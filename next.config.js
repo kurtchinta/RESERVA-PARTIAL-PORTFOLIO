40cd3ed1879cbb798b5bfa8d1be066795db0c7d9
kurtchinta <blue09908@gmail.com>
2024-10-27 15:09:52 +0800

    plugins

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
     remotePatterns: [
+      // Add any remote image patterns if needed
     ],
+    unoptimized: true,
   },
 };
